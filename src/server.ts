0df1a5a7c414325c01ba02bddf17e493bc70b7f5
Leandro Gonçalves Pena <park-luthor@Mac-mini-de-Leandro.local>
2023-02-25 11:45:46 -0300

    Up 2023-02-25 v1.5

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
+import 'dotenv/config';
 import express, { urlencoded } from 'express';
 const app = express();
 import apiRoutes from './routes/api';
 import cors from 'cors';
+const PORT = process.env.PORT || 3333;
 
 app.use(cors());
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use('/api', apiRoutes);
 
-app.listen(3333, () => {
-    console.log('Server is running on port 3333');
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
